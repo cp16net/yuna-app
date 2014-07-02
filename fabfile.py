6e942d71e672442009f8166d94047eafbc3579ef
Craig Vyvial <cp16net@gmail.com>
2014-07-02 02:16:59 -0500

    adding the fabric install cmd

---
+++ fabfile.py
@@ -0,0 +1,18 @@
+"""
+Common tools for deploy run shell and the like.
+"""
+
+import os
+
+from fabric.api import *
+from fabric.colors import *
+
+env.use_ssh_config = True
+
+
+@task
+def install():
+    """Install the node_modules dependencies"""
+    local('git submodule update --init')
+    local('python manage.py syncdb')
+    local('python manage.py migrate')
