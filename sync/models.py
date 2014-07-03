ffe1584f847258cd6d783bb25c60324a1c39f43c
Craig Vyvial <cp16net@gmail.com>
2014-07-02 23:34:03 -0500

    adding schedule to import and added to admin settings

--- sync/models.py
+++ sync/models.py
@@ -1,5 +1,8 @@
+from django.contrib import admin
 from django.db import models
 
 # Create your models here.
 class Schedule(models.Model):
     fields = ['user_id', 'schedule']
+
+admin.site.register(Schedule)
