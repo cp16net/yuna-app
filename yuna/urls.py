e8dc130fe992514b6573980bfe9df84d637bac58
Craig Vyvial <cp16net@gmail.com>
2014-07-09 22:33:09 -0500

    adding the home template with static css and js

--- yuna/urls.py
+++ yuna/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'yuna.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns = patterns('yuna.views',
+    url(r'^$', 'home', name='home'),
+#    url(r'^signin/$', auth_views.login),
+#    url(r'^signout/$', auth_views.logout),
 
     url(r'^admin/', include(admin.site.urls)),
 )
